8cc4c6577f7355207d59b88f290986683a298143
Kapas Askhat <kapas_ashat@mail.ru>
2023-05-24 19:03:44 +0600

    added prifile screen

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    SafeAreaView, View, Text
+} from 'react-native'
+
+import { styled } from 'nativewind'
+
+const StyledSave = styled(SafeAreaView)
+const StylefView = styled(View)
+const StyledText = styled(Text)
+
+
+export function Profile():JSX.Element{
+    return(
+        <StyledSave className="flex flex-1">
+            
+        </StyledSave>
+    )
+}
