9321b83b1f312b3feef7ed7cd6e55422f8824259
Kapas Askhat <kapas_ashat@mail.ru>
2023-05-24 18:57:28 +0600

    update logout

---
+++ src/screens/Logout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    SafeAreaView, View, Text
+} from 'react-native'
+
+import {
+    styled
+} from 'nativewind'
+
+const StyledSave = styled(SafeAreaView)
+
+export function Logout():JSX.Element{
+    return(
+        <StyledSave className="flex flex-1">
+            
+        </StyledSave>
+    )
+}
